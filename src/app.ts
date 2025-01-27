de27b545d232ac3af04a87684d14f65fa7908fe5
JonaLA <jonahaustin@outlook.com>
2025-01-26 18:04:59 -0700

    Connect to OpenAPI

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
+import OpenAI from 'openai'
 
-function main() {
-    console.log('Hello, world!')
+const client = new OpenAI({
+    apiKey: process.env['OPENAPI_API_KEY']
+})
+
+async function main() {
+    const stream = await client.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-4o-mini',
+        stream: true
+    })
+
+    let response = '';
+    for await (const chunk of stream) {
+        response += chunk.choices[0]?.delta?.content || ''
+    }
+    console.log(response)
 }
 
 main()
